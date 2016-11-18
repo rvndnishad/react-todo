37f6f85cbdfac2d204feea464a2f91efd94506ff
Arvind Nishad <rvndnishad@gmail.com>
2016-11-19 01:53:19 +0530

    Add playground for Spread operator

---
+++ playground/spread.js
@@ -0,0 +1,32 @@
+var names = ['pravin', 'ben'];
+var final = ['Arvind',...names];
+
+final.forEach((name)=>{
+  console.log(name);
+})
+
+
+// var person = ['Pravin', 28];
+// var personTwo = ['Jitu', 25];
+//
+// function greeting(name,age){
+//   return "Hello "+name+", you are "+age;
+// }
+//
+// console.log(greeting(...person));
+// console.log(greeting(...personTwo));
+
+// function add(a,b) {
+//   return a+b;
+// }
+// //console.log(add(3,1));
+//
+// var toAdd = [11,9,5];
+//
+// console.log(add(...toAdd));
+
+// let groupA = ['Arvind', 'Pravin'];
+// let groupB = ['Viru', 'Chandu', 'Rajani', 'Pradeep'];
+//
+// let final = [3, ...groupA];
+// console.log(final);
