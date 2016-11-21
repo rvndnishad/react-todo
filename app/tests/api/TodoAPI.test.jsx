76b2ee46e40f4cb4d5c454294461cf1a7d9a88f1
Arvind Nishad <rvndnishad@gmail.com>
2016-11-21 12:16:13 +0530

    Add filter functionality

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -48,5 +48,42 @@ describe('TodoAPI', function () {
       var actualTodos = TodoAPI.getTodos();
       expect(actualTodos).toEqual([])
     });
-  })
+  });
+
+  describe('filterTodos', function () {
+    var todos = [{
+      id:1,
+      text:'Some text',
+      completed:true
+    }, {
+      id:2,
+      text:'Some text',
+      completed:false
+    }, {
+      id:3,
+      text:'Eat Food',
+      completed:true
+    }];
+
+    it('should return all item if showCompleted is true', function () {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    });
+    it('should return non-completed todos if showCompleted is false', function () {
+      var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+      expect(filteredTodos.length).toBe(1);
+    });
+    it('should sort by completed status', function () {
+      var filteredTodos = TodoAPI.filterTodos(todos,true,'');
+      expect(filteredTodos[0].completed).toBe(false);
+    });
+    it('should filter the todos by search text', function () {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+      expect(filteredTodos.length).toBe(2);
+    });
+    it('should return all todos if search text is empty', function () {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    });
+  });
 });
